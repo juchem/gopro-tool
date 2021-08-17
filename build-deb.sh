10ae2b0dd0bea1e6af3efb7fceb82acaf1197175
Marcelo Juchem <juchem@gmail.com>
2021-08-17 15:54:33 -0500

    building Package file for apt repo

--- build-deb.sh
+++ build-deb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 SRC_DIR="$(git rev-parse --show-toplevel)"
 PACKAGE_NAME="gopro-tool"
+PACKAGE_VERSION="1.0"
 BUILD_DIR="${SRC_DIR}/out"
 OUT_DIR="${BUILD_DIR}/${PACKAGE_NAME}"
 
@@ -21,9 +22,17 @@ CURRENT_SRC_DIR="${SRC_DIR}/DEBIAN"
 CURRENT_OUT_DIR="${OUT_DIR}/DEBIAN"
 mkdir -p "${CURRENT_OUT_DIR}"
 for file in "control"; do
-  sed "s/INSTALLED_SIZE/${installed_size}/g" \
+  sed \
+    -e "s/INSTALLED_SIZE/${installed_size}/g" \
+    -e "s/PACKAGE_VERSION/${PACKAGE_VERSION}/g" \
     "${CURRENT_SRC_DIR}/${file}" \
     > "${CURRENT_OUT_DIR}/${file}"
 done
 
 dpkg-deb --build "${OUT_DIR}"
+
+# apt repo
+REPO_DIR="${BUILD_DIR}/apt-repo/debian"
+mkdir -p "${REPO_DIR}"
+cp "${BUILD_DIR}/${PACKAGE_NAME}.deb" "${REPO_DIR}/${PACKAGE_NAME}-${PACKAGE_VERSION}_all.deb"
+dpkg-scanpackages "${REPO_DIR}" | gzip -c9 > "${REPO_DIR}/Packages.gz"
